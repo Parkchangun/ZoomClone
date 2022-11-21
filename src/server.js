1825df08d7cba8ce33f54b6dd7cfdc72a9efb178
changeonPark <qkrcksrjs@gmail.com>
2022-11-21 18:13:17 +0900

    feat: catch all route

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/landing", (req, res) => res.render("landing"));
+// catch all
+app.get("/*", (req, res) => res.redirect("/"))
 
 console.log("app start");
 
